05f526d543c679e831de3fd85336bac7513ceb3e
Parker Williamson <parker.williamson@gmail.com>
2020-08-06 17:29:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   theDomHasLoaded()
-// });
- 
-// function theDomHasLoaded() {
-//   document.getElementById( "text" )
-//     .innerHTML = "This is really cool!";
-// }
 document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
 
-function theDomHasLoaded(e) {
+function theDomHasLoaded() {
    document.getElementById( "text" )
      .innerHTML = "This is really cool!";
 }
